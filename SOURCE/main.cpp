2ac88112857ce27735148b36d9de65acc5a33ba4
marcorispoli <marco.rispoli@gmail.com>
2023-06-15 14:34:58 +0200

    Avanzamenti

--- SOURCE/main.cpp
+++ SOURCE/main.cpp
@@ -25,10 +25,16 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     appLog(argc, argv, "C:\\OEM\\Gantry\\Log\\mcpu_potter.log", debugWindow::debugMessageHandler);
 
+    // Open the system configuration file
+    SYSCONFIG = new sysConfig(configFile::_CFG_READONLY);
+    if(!SYSCONFIG->isFormatCorrect()) {
+        qDebug() << " WRONG CONFIGURATION FILE FORMAT!";
+        exit(1);
+    }
 
     // Open the application config file
     CONFIG = new boardConfig();
-    SYSCONFIG = new sysConfig();
+
 
     // Initialize the WINDOW to nullptr so that in case of Operating mode
     // the application skips the WINDOW callbacks
